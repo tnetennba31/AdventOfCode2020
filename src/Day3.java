b25ab10bbf65964acf726043f79697f9080720fb
Madeline Cardinale <mc6672@cs.ship.edu>
2020-12-10 01:27:46 -0500

    solved and refactored day 3

--- src/Day3.java
+++ src/Day3.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class Day3 {
 	
+	ArrayList<String> map;
+	
 	public Day3(File f) {
 		try {
 			Scanner s = new Scanner(f);
@@ -12,22 +14,34 @@ public class Day3 {
 			while (s.hasNextLine()) {
 				map.add(s.nextLine());
 			}
-			System.out.println("Day 3, Part 1: " + treeCount(map)); // first part of the challenge
+			this.map = map;
+			
+			System.out.println("Day 3, Part 1: " + treeCountBySlope(3, 1)); // first part of the challenge
+			System.out.println("Day 3, Part 2: " + treeCount()); // second part of the challenge
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public int treeCount(ArrayList<String> map) {
-		int x = 3, y = 1;
+	public int treeCount() {
+		int a, b, c, d, e;
+		a = treeCountBySlope( 1, 1);
+		b = treeCountBySlope( 3, 1);
+		c = treeCountBySlope( 5, 1);
+		d = treeCountBySlope(7, 1);
+		e = treeCountBySlope(1, 2);
+		return a * b * c * d * e;
+	}
+	
+	public int treeCountBySlope(int right, int down) {
+		int x = right;
 		int trees = 0;
-		for (int i = 0; i < map.size() - 1; i++) {
+		for (int y = down; y < map.size(); y+=down) {
 			String s = map.get(y);
 			if (x >= s.length()) x = x % s.length(); // map repeats, so loop back if need more columns
 			char symbol = s.charAt(x);
 			if (symbol == '#') trees++;
-			x += 3;
-			y += 1;
+			x += right;
 		}
 		return trees;
 	}
