d615ee9ec4ab8d9e0f6adf1c9870ea7e5f0b3201
Madeline Cardinale <mc6672@cs.ship.edu>
2020-12-09 23:53:27 -0500

    finished day 1, two gold stars in the bank

--- src/Day1.java
+++ src/Day1.java
@@ -1,14 +1,22 @@
 import java.io.File;
-import java.lang.reflect.Array;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Day1 {
 	
 	public Day1(File f) {
 		ArrayList<Integer> list = new ArrayList<>();
-		
-		
+		try {
+			Scanner s = new Scanner(f);
+			while (s.hasNext()) {
+				list.add(s.nextInt());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		System.out.println(sumTo2020(list));
+		System.out.println(sumTo2020Again(list));
 	}
 	
 	public static int sumTo2020(ArrayList<Integer> nums) {
@@ -17,6 +25,19 @@ public class Day1 {
 			int diff = 2020 - x;
 			if (nums.contains(diff)) {
 				winner = diff * x;
+			}
+		}
+		return winner;
+	}
+	
+	public static int sumTo2020Again(ArrayList<Integer> nums) {
+		int winner = 0;
+		for (int x : nums) {
+			for (int y : nums) {
+				int diff = 2020 - x - y;
+				if ((y != x) && (nums.contains(diff))) {
+					winner = x * y * diff;
+				}
 			}
 		}
 		return winner;
--- src/runner.java
+++ src/runner.java
@@ -1,9 +1,8 @@
 import java.io.File;
-import java.util.ArrayList;
 
 public class runner {
 	
 	public static void main(String[] args) {
-		new Day1(new File("input/day1.txt"));
+		new Day1(new File("src/input/day1.txt"));
 	}
 }
