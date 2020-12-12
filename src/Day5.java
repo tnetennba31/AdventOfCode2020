8bdf24ca6e41a2a3d6bbfd6d46e0e5f282f1b6c0
Madeline Cardinale <mc6672@cs.ship.edu>
2020-12-12 12:55:55 -0500

    wowzers that was rough

--- src/Day5.java
+++ src/Day5.java
@@ -8,39 +8,24 @@ public class Day5 {
 	public Day5(File f) {
 		try {
 			Scanner s = new Scanner(f);
-			ArrayList<String> passes = new ArrayList<>();
+			ArrayList<Integer> passes = new ArrayList<>();
+			// this loop came from reddit because i never would have thought to do it this way
 			while (s.hasNextLine()) {
-				passes.add(s.nextLine());
+				String binary = s.nextLine().replaceAll("[FL]", "0").replaceAll("[BR]", "1");
+				passes.add(Integer.parseInt(binary, 2));
 			}
-			System.out.println("Day 5, Part 1: " + getHighestID(passes));	// first part of the challenege
+			passes.sort(null);
+			System.out.println("Day 5, Part 1: " + passes.get(passes.size() - 1));  // first part of the challenege
+			System.out.println("Day 5, Part 2: " + findMySeat(passes));  // second part of the challenge
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public int getHighestID(ArrayList<String> passes) {
-		int highest = 0;
-		for (String p : passes) {
-			highest = Math.max(highest, calculateID(p));
+	public int findMySeat(ArrayList<Integer> IDs) {
+		for (int i : IDs) {
+			if (!IDs.contains(i+1) && IDs.contains(i+2)) return i+1;
 		}
-		return highest;
-	}
-	
-	public int calculateID(String p) {
-		int highRow = 127;
-		int lowRow = 0;
-		int highCol = 7;
-		int lowCol = 0;
-		for (char c : p.toCharArray()) {
-			int rowOptions = (highRow - lowRow + 1) / 2;	// how many options we have left for the row
-			int colOptions = (highCol - lowCol + 1) / 2;	// how many options we have left for the column
-			switch (c) {
-				case 'F' -> highRow = lowRow + rowOptions - 1;	// choose lower half of rows
-				case 'B' -> lowRow = highRow - rowOptions + 1;	// choose upper half of rows
-				case 'R' -> highCol = lowCol + colOptions - 1;	// choose lower half of columns
-				case 'L' -> lowCol = highCol - colOptions + 1;	// choose upper half of columns
-			}
-		}
-		return highRow * 8 + highCol;
+		return 0;
 	}
 }
